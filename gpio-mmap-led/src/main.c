560399fc050ce861318717465954c3cad7478dc8
Raashid Muhammed <raashidmuhammed@feostech.com>
2024-06-08 06:33:01 +0530

    Code improvements.

--- gpio-mmap-led/src/main.c
+++ gpio-mmap-led/src/main.c
@@ -1,20 +1,30 @@
-#include <zephyr/zephyr.h>
+#include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
 
 #define GPIO_P0_BASE_ADDR 0x50000000
-#define GPIO_P0_SET_DIR_REG (GPIO_P0_BASE_ADDR + 0x518)
+#define GPIO_P0_OUTPUT_DIR_REG (GPIO_P0_BASE_ADDR + 0x518)
 #define GPIO_P0_SET_BIT_REG (GPIO_P0_BASE_ADDR + 0x508)
 #define GPIO_P0_CLEAR_BIT_REG (GPIO_P0_BASE_ADDR + 0x50C)
 #define GPIO_P0_2 (1 << 2)
 
-void main(void)
+#define DELAY_MS 1000
+
+volatile uint32_t *led_dir = (uint32_t *)GPIO_P0_OUTPUT_DIR_REG;
+volatile uint32_t *led_set_val = (uint32_t *)GPIO_P0_SET_BIT_REG;
+volatile uint32_t *led_clear_val = (uint32_t *)GPIO_P0_CLEAR_BIT_REG;
+
+int main(void)
 {
-	*(volatile uint32_t *)GPIO_P0_SET_DIR_REG = (uint32_t)GPIO_P0_2;
+  	/* Configure LED as input */
+	*led_dir |= (uint32_t)GPIO_P0_2;
+
 	while (1) {
-		*(volatile uint32_t *)GPIO_P0_SET_BIT_REG = (uint32_t)GPIO_P0_2;
-		k_msleep(1000);
-		*(volatile uint32_t *)GPIO_P0_CLEAR_BIT_REG = (uint32_t)GPIO_P0_2;
-		k_msleep(1000);
+		*led_set_val |= (uint32_t)GPIO_P0_2;
+		k_msleep(DELAY_MS);
+		*led_clear_val |= (uint32_t)GPIO_P0_2;
+		k_msleep(DELAY_MS);
 	}
+
+	return 0;
 }
